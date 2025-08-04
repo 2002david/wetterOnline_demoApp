113bdd1e3184c79dfeaea04de69cbe91062f0ad2
2002david <david.steglich@web.de>
2025-08-04 18:10:10 +0200

    Initial Commit

---
+++ wetterOnline_demoApp/wetterOnline_demoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  wetterOnline_demoAppApp.swift
+//  wetterOnline_demoApp
+//
+//  Created by David on 04.08.25.
+//
+
+import SwiftUI
+
+@main
+struct wetterOnline_demoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wetterOnline_demoAppTests/wetterOnline_demoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  wetterOnline_demoAppTests.swift
+//  wetterOnline_demoAppTests
+//
+//  Created by David on 04.08.25.
+//
+
+import Testing
+@testable import wetterOnline_demoApp
+
+struct wetterOnline_demoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
